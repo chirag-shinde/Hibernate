7bddd854d7998fdca5a592850239b0c9b8ef37d9
Chirag Shinde <shinde.chirag2204@gmail.com>
2016-10-12 15:49:39 +0530

    small updates

--- src/main/java/com/smartify/model/Application.java
+++ src/main/java/com/smartify/model/Application.java
@@ -1,12 +1,27 @@
 package com.smartify.model;
 
+import java.util.Date;
+
 import org.hibernate.Session;
+
+import com.smartify.model.entities.User;
 
 public class Application {
 	public static void main(String[] args) {
 		Session session = HibernateUtil.getSessionfactory().openSession();
 		session.beginTransaction();
 		
+		User user = new User();
+		user.setBirthDate(new Date());
+		user.setCreatedBy("John Doe");
+		user.setCreatedDate(new Date());
+		user.setEmailAddress("user@user.com");
+		user.setFirstName("User");
+		user.setLastname("TestUser");
+		user.setLastUpdatedBy("John Doe");
+		user.setLastUpdatedDate(new Date());
+		
+		session.save(user);
 		session.getTransaction().commit();
 		session.close();
 		HibernateUtil.getSessionfactory().close();

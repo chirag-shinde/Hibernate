d75c5e3a5b8658a2a56df2df7b3bd212b1b36cef
Chirag Shinde <shinde.chirag2204@gmail.com>
2016-10-13 10:51:09 +0530

    added try catch block

--- src/main/java/com/smartify/model/Application.java
+++ src/main/java/com/smartify/model/Application.java
@@ -3,39 +3,45 @@ package com.smartify.model;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import com.smartify.model.entities.User;
 
 public class Application {
 	public static void main(String[] args) {
-		Session session = HibernateUtil.getSessionfactory().openSession();
-		session.beginTransaction();
-		
-		User user = new User();
-		user.setBirthDate(getBirthday());
-		user.setCreatedBy("John Doe");
-		user.setCreatedDate(new Date());
-		user.setEmailAddress("chirag@shinde.com");
-		user.setFirstName("Chirag");
-		user.setLastname("Shinde");
-		user.setLastUpdatedBy("John");
-		user.setLastUpdatedDate(new Date());
-		
-		session.save(user);
-		session.getTransaction().commit();
-		
-		session.refresh(user);
-		
-		System.out.println(user.getAge() );
-		session.close();
-		HibernateUtil.getSessionfactory().close();
+		try (Session session = HibernateUtil.getSessionfactory().openSession()) {
+
+			session.beginTransaction();
+
+			User user = new User();
+			user.setBirthDate(getBirthday());
+			user.setCreatedBy("John Doe");
+			user.setCreatedDate(new Date());
+			user.setEmailAddress("chirag@shinde.com");
+			user.setFirstName("Chirag");
+			user.setLastname("Shinde");
+			user.setLastUpdatedBy("John");
+			user.setLastUpdatedDate(new Date());
+
+			session.save(user);
+			session.getTransaction().commit();
+
+			session.refresh(user);
+
+			System.out.println(user.getAge());
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		} finally {
+			HibernateUtil.getSessionfactory().close();
+		}
 	}
-	private static Date getBirthday(){
+
+	private static Date getBirthday() {
 		Calendar calender = Calendar.getInstance();
-		calender.set(Calendar.YEAR,1993);
-		calender.set(Calendar.MONTH,4);
-		calender.set(Calendar.DATE,22);
+		calender.set(Calendar.YEAR, 1993);
+		calender.set(Calendar.MONTH, 4);
+		calender.set(Calendar.DATE, 22);
 		return calender.getTime();
 	}
 }
